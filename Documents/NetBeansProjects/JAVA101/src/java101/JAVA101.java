c6f21a63d24105abc7f0dc4605c0bb2c53f3a121
Java <Java@Java-PC>
2014-06-25 13:19:19 -0800

    new commit

---
+++ Documents/NetBeansProjects/JAVA101/src/java101/JAVA101.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package java101;
+
+/**
+ *
+ * @author Java
+ */
+public class JAVA101 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        TEST test = new TEST();
+        test.printText();
+    }
+    
+}
---
+++ Documents/NetBeansProjects/JAVA101/src/java101/TEST.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package java101;
+
+/**
+ *
+ * @author Java
+ */
+public class TEST {
+public String text;
+    public TEST() {
+        this.text = " Hello !";
+    }
+    public void printText()
+    {
+        System.out.println(this.text);
+    }
+    
+}
