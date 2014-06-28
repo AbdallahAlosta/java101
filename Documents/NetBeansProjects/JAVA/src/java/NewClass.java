4f9a6d8d111cd54e509cff5017f9e0ffc69dfc89
Java <Java@Java-PC>
2014-06-27 23:20:53 -0800

    I change to 800

---
+++ Documents/NetBeansProjects/JAVA/src/java/NewClass.java
@@ -0,0 +1,12 @@
+
+public class NewClass {
+    public int val;
+    public NewClass() {
+        this.val = 9;
+    }
+    public void printHello()
+    {
+        System.out.println(" Welcome To !");
+        
+    }
+}
