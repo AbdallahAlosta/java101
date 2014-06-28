060b60597dc3ea41ee4f9d556149cc6f67b2ceca
Java <Java@Java-PC>
2014-06-27 23:35:00 -0800

    hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- Documents/NetBeansProjects/HI/src/hi/HI.java
+++ Documents/NetBeansProjects/HI/src/hi/HI.java
@@ -2,18 +2,12 @@
 
 package hi;
 
-/**
- *
- * @author Java
- */
 public class HI {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println(" Welcome !");
+        System.out.println("hh");
     }
     
 }
